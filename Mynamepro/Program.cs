d41cbc84c715a4f677cb0ac22456f5f02cee9a4f
Usuario <Usuario@DESKTOP-D7TBL2I>
2022-02-24 18:35:19 -0800

    Agregar archivos de proyecto.

---
+++ Mynamepro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CalculoArea
+{
+    internal class program
+    {
+        static void Main(string[] args)
+
+            {
+
+            int a = 10;
+            int b = 10;
+            int Area = (a * b / 2);
+
+            Console.WriteLine("El Area del triangulo es: + Area");
+            Console.WriteLine("Oprime una tecla para finalizar");
+            Console.ReadKey();
+            
+
+
+        }
+      }
+    }
